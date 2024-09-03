022861e7c3776412f30e2d7be02ed14aff6c9646
FlipR-data <flipr2024@gmail.com>
2024-09-03 09:59:59 +0200

    UPDATE shot_map, add factor shotType

--- shot_map.R
+++ shot_map.R
@@ -5,11 +5,28 @@ library(ggsoccer)
 library(patchwork)
 library(ggtext)
  
-mbappe  <- understat_player_shots(player_url = "https://understat.com/player/3423")  %>% filter(season  == 2023,result=="Goal")
-rodri   <- understat_player_shots(player_url = "https://understat.com/player/2496")  %>% filter(season  == 2023,result=="Goal")
-kane    <- understat_player_shots(player_url = "https://understat.com/player/647")   %>% filter(season  == 2023,result=="Goal")
-haaland <- understat_player_shots(player_url = "https://understat.com/player/8260")  %>% filter(season  == 2023,result=="Goal")
+mbappe  <- understat_player_shots(player_url = "https://understat.com/player/3423")  %>% 
+           filter(season  == 2023,result=="Goal")                                    %>% 
+           mutate(shotType = as.factor(shotType))
+rodri   <- understat_player_shots(player_url = "https://understat.com/player/2496")  %>% 
+           filter(season  == 2023,result=="Goal")                                    %>% 
+           mutate(shotType = as.factor(shotType))
+kane    <- understat_player_shots(player_url = "https://understat.com/player/647")   %>% 
+           filter(season  == 2023,result=="Goal")                                    %>% 
+           mutate(shotType = as.factor(shotType))
+haaland <- understat_player_shots(player_url = "https://understat.com/player/8260")  %>% 
+           filter(season  == 2023,result=="Goal")                                    %>% 
+           mutate(shotType = as.factor(shotType))
  
+
+shotType_levels <- c("Head", "LeftFoot","OtherBodyPart","RightFoot")
+mbappe$shoTypeT<-factor(mbappe$shotType,levels=shotType_levels )
+rodri$shoTypeT<-factor(rodri$shotType,levels=shotType_levels )
+kane$shoTypeT<-factor(kane$shotType,levels=shotType_levels )
+haaland$shoTypeT<-factor(haaland$shotType,levels=shotType_levels )
+
+
+
 map_shot <- function(data,player_name) {
   
    data <- data  %>%  mutate(x=X*100,y=Y*100)     
@@ -17,11 +34,15 @@ map_shot <- function(data,player_name) {
    
    res_shot<- ggplot(data) +
               annotate_pitch(colour = "white", fill = "springgreen4",  limits = FALSE)          +
-              geom_jitter(aes(x = x, y = y,colour = shotType), width = 1, height =  1,size = 1) +
+              geom_jitter(aes(x = x, y = y,colour = shoTypeT), width = 1, height =  1,size = 2) +
+              scale_fill_manual(
+              values = c("Head", "LeftFoot","OtherBodyPArt","RightFoot"))                       +
               theme_pitch()                                                                     +
               theme(panel.background = element_rect(fill = "springgreen4"))                     +
               coord_flip(xlim = c(49, 101))                                                     +
               scale_y_reverse()                                                                 +
+              scale_color_discrete(drop = FALSE)                                                +  
+              scale_size_discrete(drop = FALSE)                                                 + 
               ggtitle(player_name,   paste0("Number of goals:",n))                              + 
               theme(panel.border = element_rect(color = "black", fill = NA, size = 3))          + 
               theme(plot.title = element_text(hjust = 0.5))    
@@ -29,10 +50,11 @@ map_shot <- function(data,player_name) {
   return(res_shot)
 }
  
-plot1 <- map_shot(mbappe ,"Kylian Mbappe-Lottin") + guides(colour = "none")
-plot2 <- map_shot(rodri  ,"Rodri")                + guides(colour = "none")
-plot3 <- map_shot(kane   ,"Harry Kane")           + guides(colour = "none")
+plot1 <- map_shot(mbappe  ,"Kylian Mbappe-Lottin") + guides(colour = "none")
+plot2 <- map_shot(rodri   ,"Rodri")                + guides(colour = "none")
+plot3 <- map_shot(kane    ,"Harry Kane")           + guides(colour = "none")
 plot4 <- map_shot(haaland ,"Erling Haaland")  
+
 
 ###Final display in a 2x2 presentation
 overall<-plot1 + plot2 + plot3 + plot4  + plot_layout(guides = "collect") +
@@ -73,7 +95,6 @@ map_shot_v <- function(data,player_name) {
                    label3 = player_assisted,
                    label4 = match_), 
                size = 2, colour = "blue") +
-    
     theme_pitch() +
     theme(panel.background = element_rect(fill = "springgreen4")) +
     coord_flip(xlim = c(49, 101)) +
